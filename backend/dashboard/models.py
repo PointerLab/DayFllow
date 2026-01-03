642467f49f728f4d3d963e05e84ee22b691c73e3
manishhsuthar <manishsuthar.dev@gmail.com>
2026-01-03 16:46:20 +0530

    signup issue

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
